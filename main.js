d66b5f4317ca94c3c136d4abb0b87be4d3c296a9
anhdk581998 <anh0941750335@gmail.com>
2023-08-23 04:21:12 +0700

    hoàn thành dự án

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ function showProducts() {
             <img src="${productsInStore[i].img}" alt="">
             <span>${productsInStore[i].category}</span>
             <h2>${productsInStore[i].name}</h2>
-            <h3 class="price">7000vnd <span>/kg</span></h3>
+            <h3 class="price">${productsInStore[i].price}<span>/kg</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+            <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">${productsInStore[i].discount}</span>
         </div>
 `
@@ -38,7 +38,7 @@ function showProducts() {
             <h2>Đào Tấn <br>1 quả 250g </h2>
             <h3 class="price">10,000vnd <span>/kg</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+                 <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>
@@ -49,7 +49,7 @@ function showProducts() {
             <h2>Ngũ cốc ăn kiêng <br>fruits 250g </h2>
             <h3 class="price">7000vnd <span>/kg</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+            <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>
@@ -60,7 +60,7 @@ function showProducts() {
             <h2>Trà Đào Cam Sả <br> cốc </h2>
             <h3 class="price">25,000vnd <span>/cốc</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+             <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>
@@ -71,7 +71,7 @@ function showProducts() {
             <h2>Xà lách <br>1 bó </h2>
             <h3 class="price">7,000vnd <span>/bó</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+            <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>
@@ -82,7 +82,7 @@ function showProducts() {
             <h2>Ớt chỉ thiên <br>1 túi 250g </h2>
             <h3 class="price">15,000vnd <span>/kg</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+            <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>
@@ -93,12 +93,15 @@ function showProducts() {
             <h2>Chuối tiêu thái <br> 250g </h2>
             <h3 class="price">27,900vnd <span>/kg</span></h3>
             <i class='bx bxs-message-square-x' onclick="remove()"></i>
-            <i class='bx bx-heart'></i>
+            <a href="#editProduct"  onclick="showFormEdit(${i})"><i class='bx bx-edit'></i></a>
             <span class="discount">
           -25%</span>
         </div>`
         +
         `<div class="" id="inputProduct">
+        </div>`
+    +
+        `<div class="" id="editProduct">
         </div>`
     ;
 }
@@ -106,14 +109,14 @@ function showProducts() {
 function showFormAdd() {
     document.getElementById('inputProduct').innerHTML=`
                         <div class="box">
-                            <h2>nhập ảnh sản phẩm<input type="text" id="imgInput"></h2>
-                            <span class="fruit" id="fruit1">Nhập loại sản phẩm <input type="text" id="categoryInput"></span>
-                            <h2>nhập mô tả sản phẩm<input id="nameInput"></h2>
-                            <h3 class="price">nhập giá sản phẩm: <input type="text" id="priceInput"> <span>/kg</span></h3>
+                            <h2>Ảnh sản phẩm: <br>    <input type="text" id="imgInput"></h2>
+                            <span class="fruit" id="fruit1">Loại sản phẩm: <br>      <input type="text" id="categoryInput"></span>
+                            <h2>Mô tả sản phẩm<input id="nameInput"></h2>
+                            <h3 class="price">Giá sản phẩm: <br>      <input type="text" id="priceInput"> <span>/kg</span></h3>
                             
                           
-                            <span class="">Nhập khuyến mãi:<input type="text" name="" id="discountInput"></span>
-                            <a href="" class="btn" onclick="add()">Add<i class='bx bxs-right-arrow'></i></a>
+                            <span class="">Khuyến mãi: <br>     <input type="text" name="" id="discountInput"><br></span>
+                            <a href="" class="btn1" onclick="add()">  <br> ADD<i class='bx bxs-right-arrow'></i></a>
                         </div>
     `
 }
@@ -138,3 +141,37 @@ function add() {
     document.getElementById('inputProduct').innerHTML = '';
 }
 
+function edit(i) {
+    let img = document.getElementById('imgInput2').value;
+    let category = document.getElementById('categoryInput2').value;
+    let name = document.getElementById('nameInput2').value;
+    let price = document.getElementById('priceInput2').value;
+    let discount = document.getElementById('discountInput2').value;
+    let editProduct = new Product (img, category, name, price, discount);
+    store.edit(i,editProduct);
+    console.log(store)
+    showProducts();
+    document.getElementById('inputProduct').innerHTML = '';
+}
+function showFormEdit(i) {
+    document.getElementById('inputProduct').innerHTML=`
+                        <div class="box">
+                            <h2>Ảnh sản phẩm: <br>    <input type="text" value="${productsInStore[i].img}" id="imgInput2"></h2>
+                            <span class="fruit" id="fruit1">Loại sản phẩm:<br><input type="text" value="${productsInStore[i].category}" id="categoryInput2"></span>
+                            <h2>Mô tả sản phẩm<input type="text" value="${productsInStore[i].name}"  id="nameInput2"></h2>
+                            <h3 class="price">Giá sản phẩm: <br><input type="text" value="${productsInStore[i].price}" id="priceInput2"> <span>/kg</span></h3>                     
+                            <span class="">Khuyến mãi: <br>     <input type="text" value="${productsInStore[i].discount}" id="discountInput2"><br></span>
+                            <a href="" class="btn1" onclick="edit(${i})">  <br> Sửa<i class='bx bxs-right-arrow'></i></a>
+                        </div>
+    `
+}
+
+// <div className="box">
+//     <img src="${productsInStore[i].img}" alt="">
+//         <span>${productsInStore[i].category}</span>
+//         <h2>${productsInStore[i].name}</h2>
+//         <h3 className="price">7000vnd <span>/kg</span></h3>
+//         <i className='bx bxs-message-square-x' onClick="remove()"></i>
+//         <i className='bx bx-heart'></i>
+//         <span className="discount">${productsInStore[i].discount}</span>
+// </div>
